32c3ef781407935caded596ecade2db8634c179b
regimental12 <richard.hankins@gmail.com>
2017-01-31 11:09:28 +0000

    initial commit

---
+++ app/src/main/java/com/indievoodoo/richard/weightliftingtracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.indievoodoo.richard.weightliftingtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
