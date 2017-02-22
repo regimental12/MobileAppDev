916b17c738b0a88a0f1677502c2cf271b87a70a3
regimental12 <richard.hankins@gmail.com>
2017-02-22 14:07:22 +0000

    got graph working

--- app/src/main/java/com/indievoodoo/richard/weightliftingtracker/ChartActivity.java
+++ app/src/main/java/com/indievoodoo/richard/weightliftingtracker/ChartActivity.java
@@ -1,37 +1,40 @@
 package com.indievoodoo.richard.weightliftingtracker;
 
 import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.graphics.Color.RED;
+import static android.graphics.Color.WHITE;
 
 
 // TODO get data from database.
 // TODO format graph better.
 // TODO Create tabed layout1 for each graph.
 
-public class ChartActivity extends AppCompatActivity
+public class ChartActivity extends FragmentActivity
 {
-
     LinearLayout layout1;
-    LinearLayout layout3;
-    LinearLayout layout2;
-    LineChart lChart1 , lChart2 , lChart3;
+    LineChart lChart1;
 
-    private float[] yData = { 5, 10, 15, 30, 40 };
-    private String[] xData = { "Sony", "Huawei", "LG", "Apple", "Samsung" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -40,39 +43,20 @@ public class ChartActivity extends AppCompatActivity
         setContentView(R.layout.activity_chart);
 
         // setup layout1 and chart
-        layout1 = (LinearLayout) findViewById(R.id.tab1);
-        layout2 = (LinearLayout) findViewById(R.id.tab2);
-        layout3 = (LinearLayout) findViewById(R.id.tab3);
+        layout1 = (LinearLayout) findViewById(R.id.chart_lin);
+
         lChart1 = new LineChart(this);
-        lChart2 = new LineChart(this);
-        lChart3 = new LineChart(this);
+
 
         // add chart to layout1
         layout1.addView(lChart1);
-        layout1.setBackgroundColor(Color.parseColor("#d3d3d3"));
 
-        /*layout2.addView(lChart2);
-        layout2.setBackgroundColor(Color.parseColor("#d3d3d3"));
+        ViewGroup.LayoutParams params = lChart1.getLayoutParams();
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
 
-        layout3.addView(lChart3);
-        layout3.setBackgroundColor(Color.parseColor("#d3d3d3"));*/
-
-        // config chart
-        //lChart1.setDescription();
-
-        ViewGroup.LayoutParams params2 = lChart1.getLayoutParams();
-        params2.height = ViewGroup.LayoutParams.MATCH_PARENT;
-        params2.width = ViewGroup.LayoutParams.MATCH_PARENT;
-
-        /*ViewGroup.LayoutParams params1 = lChart2.getLayoutParams();
-        params1.height = ViewGroup.LayoutParams.MATCH_PARENT;
-        params1.width = ViewGroup.LayoutParams.MATCH_PARENT;
-
-        ViewGroup.LayoutParams params3 = lChart3.getLayoutParams();
-        params3.height = ViewGroup.LayoutParams.MATCH_PARENT;
-        params3.width = ViewGroup.LayoutParams.MATCH_PARENT;*/
-
-
+        lChart1.setGridBackgroundColor(WHITE);
+        lChart1.setDrawGridBackground(false);
         addWeights();
 
     }
@@ -80,60 +64,30 @@ public class ChartActivity extends AppCompatActivity
     private void addWeights()
     {
         DBTools dbtools = new DBTools(this);
-        ArrayList<Graphresult> graphresults = new ArrayList<>();
-        graphresults = dbtools.getWeightsForGraph();
+        ArrayList<Graphresult> graphresults = dbtools.getWeightsForGraph();
 
-        ArrayList<Entry> yVals = new ArrayList<>();
         ArrayList<Entry> xVals = new ArrayList<>();
 
         for (int i = 0 ; i < graphresults.size() ; i++)
         {
-            yVals.add(new Entry(graphresults.get(i).getWorkoutNo() , i));
-            xVals.add(new Entry(graphresults.get(i).getWeight() , i));
+            xVals.add(new Entry(i ,graphresults.get(i).getWeight()));
         }
 
-        LineDataSet xDataSet = new LineDataSet(xVals , "WorkOuT No.");
-        //xDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-        LineDataSet yDataSet = new LineDataSet(yVals , "Weight.");
+        LineDataSet xDataSet = new LineDataSet(xVals , "Weight");
+        xDataSet.setAxisDependency(YAxis.AxisDependency.RIGHT);
+        xDataSet.setColor(RED);
+        xDataSet.setDrawCircles(false);
+        xDataSet.setLineWidth(2.0f);
 
-        List<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-        dataSets.add(xDataSet);
-        dataSets.add(yDataSet);
-
-        LineData data = new LineData(dataSets);
+        LineData data = new LineData(xDataSet);
 
         lChart1.setData(data);
 
         lChart1.invalidate();
     }
 
-    private void addValues()
-    {
-        ArrayList<Entry> yVals = new ArrayList<>();
-
-        for (int i = 0; i < yData.length; i++)
-            yVals.add(new Entry(yData[i], i));
-
-        ArrayList<String> xVals = new ArrayList<String>();
-
-        for (int i = 0; i < xData.length; i++)
-            xVals.add(xData[i]);
 
 
-
-        LineDataSet dataSet = new LineDataSet(yVals , "Market Share");
-
-        LineData data = new LineData(dataSet);
-
-        lChart1.setData(data);
-        lChart2.setData(data);
-        lChart3.setData(data);
-
-        lChart1.invalidate();
-        lChart2.invalidate();
-        lChart3.invalidate();
-
-    }
 
 
 }
